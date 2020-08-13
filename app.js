d679414f1798bc96b36ba5607b7d7663b784f4ed
parsa <parsa_monfared20@yahoo.com>
2020-08-13 16:59:50 +0200

    commit

--- app.js
+++ app.js
@@ -1,32 +1,30 @@
+// ! 1- Reuqirements : -------------------------------------------
 
-// ! 1- Reuqirements : ------------------------------------------
-
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 const port = process.env.PORT || 3000;
 
 // ! 2- Settings : ------------------------------------------
 
-app.set('view engine','ejs');
-app.use('/public', express.static('public'));
+app.set("view engine", "ejs");
+app.use("/public", express.static("public"));
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // ! 3- Show Home Page : ------------------------------------------
 
-app.get('/',(req,res)=>{
-    res.render('index');
+app.get("/", (req, res) => {
+  res.render("index");
 });
 
 // ! 4- Post Email : ------------------------------------------
 
-app.post('/send', ( req,res)=>{
-  
+app.post("/send", (req, res) => {
   let output = `
     <h1>New Email</h1>
       <ul>
@@ -37,7 +35,6 @@ app.post('/send', ( req,res)=>{
       </ul>
 
   `;
-
 });
 
 // ! 5- NodeMailer : ------------------------------------------
@@ -46,9 +43,8 @@ app.post('/send', ( req,res)=>{
 
 // ! 5.1 - Global Settings for Email Service :
 let transporter = nodemailer.createTransport({
-
   // 1st Way for Gmail:
-  service :'gmail',
+  service: "gmail",
 
   // 2nd Way :
   // host: 'stmp.example.com',
@@ -56,39 +52,37 @@ let transporter = nodemailer.createTransport({
 
   secure: false,
 
-  auth:{
-    user: 'aece@dci.org', // write your email address
-    pass: '12335kskl' // write your password here!
+  auth: {
+    user: "aece@dci.org", // write your email address
+    pass: "12335kskl", // write your password here!
   },
   tls: {
-    rejectUnauthorized: false
-  }
+    rejectUnauthorized: false,
+  },
 });
 
 // ! 5.2 - Defines your Email Details :
 
 let mailOptions = {
-
   from: '"Nodemailer Contact" <aece@dci.org>', // Sender address
-  to: 'aece@digitalcareerinstitute.org', // list of receivers
-  subject: 'Node Contact Request', // plain text body 
-  text: 'Hello FbW10 :)',
-  html: output // html body
-}
+  to: "aece@digitalcareerinstitute.org", // list of receivers
+  subject: "Node Contact Request", // plain text body
+  text: "Hello FbW10 :)",
+  html: output, // html body
+};
 
 // ! 5.3 - Send Mail with defined transport object :
 
-transporter.sendMail(mailOptions, (error, info)=>{
-  if(error){
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
     return console.log(error);
   }
 
-  console.log('Message sent:', info.messageID);
+  console.log("Message sent:", info.messageID);
 
-  res.render('index','Email has been sent');
+  res.render("index", "Email has been sent");
 });
 
 // ! Listen Port : ---------------------------------------------
 
-app.listen(port, ()=> console.log(`App running on port ${port}`));
-
+app.listen(port, () => console.log(`App running on port ${port}`));
